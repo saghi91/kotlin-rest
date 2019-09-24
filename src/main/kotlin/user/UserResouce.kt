141eca61888e51f618fcbb0ae00580c5e2bc3dcf
Saghi Ghorbani <saghi.ghorbani@metronom.com>
2019-09-24 15:39:56 +0200

    complete user CRUD implementation

--- src/main/kotlin/user/UserResouce.kt
+++ src/main/kotlin/user/UserResouce.kt
@@ -1,15 +1,32 @@
 package user
 
-import javax.ws.rs.PathParam
+import javax.ws.rs.*
 
 class UserResouce {
     private val users = HashMap<String, User>()
 
     init {
-        users += "Saghi" to User("Saghi", "12345", 20)
+        users += Pair("Saghi", User("Saghi", "123456", 20))
     }
 
-    fun getUser(@PathParam("username") username: String): User? {
+    @GET @Path("{username}")
+    fun get(@PathParam("username") username: String): User? {
         return users[username]
+    }
+
+    @POST
+    fun create(user: User) {
+        users += user.username to user
+    }
+
+    @PUT @Path("{username}")
+    fun update(@PathParam("username") username: String, user: User) {
+        users -= username
+        users += user.username to user
+    }
+
+    @DELETE @Path("{username}")
+    fun delete(@PathParam("username") username: String): User? {
+        return users.remove(username)
     }
 }
